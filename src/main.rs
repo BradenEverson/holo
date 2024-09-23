4b6e71379eb24c3776366122559929557b08ad97
BradenEverson <bradeneverson@gmail.com>
2024-09-23 12:49:55 -0500

    Update BL

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 24_000_000, Mode::Mode0)
         .expect("Failed to initialize SPI");
 
-    let mut lcd = St7789Lcd::new(25, 24, 18, spi, 240, 240).expect("Failed to initialize LCD Screen");
+    let mut lcd = St7789Lcd::new(25, 24, 23, spi, 240, 240).expect("Failed to initialize LCD Screen");
     lcd.init();
 
     lcd.draw_image("img/frank.png").expect("Failed to write image to lcd");   
