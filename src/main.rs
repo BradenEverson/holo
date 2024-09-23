6693dcd281eea549090ab3f9a148148851df0fb7
BradenEverson <bradeneverson@gmail.com>
2024-09-23 09:47:28 -0500

    Invert img

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use holo::lcd::Ili9341Lcd;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 
 fn main() {
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 15_000_000, Mode::Mode3)
+    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 24_000_000, Mode::Mode0)
         .expect("Failed to initialize SPI");
 
-    let mut lcd = Ili9341Lcd::new(25, 24, 23, spi, 240, 240).expect("Failed to initialize LCD Screen");
+    let mut lcd = Ili9341Lcd::new(25, 24, 18, spi, 240, 240).expect("Failed to initialize LCD Screen");
     lcd.init();
 
     lcd.draw_image("img/test.png").expect("Failed to write image to lcd");   
