5614db94596620131fabfd4a4d7ec7d0468bf58a
BradenEverson <bradeneverson@gmail.com>
2024-09-24 13:37:18 -0500

    Add startup

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@ use holo::lcd::St7789Lcd;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 
 fn main() {
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 24_000_000, Mode::Mode0)
+    // Setup
+    for _ in 0..10 {
+        let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 24_000_000, Mode::Mode3)
+            .expect("Failed to initialize SPI");
+
+        let mut lcd = St7789Lcd::new(25, 24, 18, spi, 240, 240).expect("Failed to initialize LCD Screen");
+        lcd.init();
+    }
+
+    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 24_000_000, Mode::Mode3)
         .expect("Failed to initialize SPI");
 
-    let mut lcd = St7789Lcd::new(25, 24, 23, spi, 240, 240).expect("Failed to initialize LCD Screen");
+    let mut lcd = St7789Lcd::new(25, 24, 18, spi, 240, 240).expect("Failed to initialize LCD Screen");
     lcd.init();
 
-    lcd.draw_image("img/frank.png").expect("Failed to write image to lcd");   
+    lcd.draw_image("img/test.png").expect("Failed to write image to lcd");   
 }
 
