dbc7cade6ae7e16c16935b5c3e86010c7bcc1962
BradenEverson <bradeneverson@gmail.com>
2024-09-30 04:59:49 -0500

    Cargo fmt

--- holo-client/src/lcd.rs
+++ holo-client/src/lcd.rs
@@ -46,6 +46,10 @@ impl St7789Lcd {
         self.send_image(&img.to_rgb8())
     }
 
+    fn osidf() {
+        ImageBuffer::from_fn(width, height, f);
+    }
+
     pub fn send_image(
         &mut self,
         img: &ImageBuffer<Rgb<u8>, Vec<u8>>,
@@ -84,73 +88,73 @@ impl St7789Lcd {
         sleep(Duration::from_millis(100));
 
         self.lcd_command(0x36); // Memory Data Access Control
-        self.lcd_data(0x00);    // Set rotation, RGB format
+        self.lcd_data(0x00); // Set rotation, RGB format
 
         self.lcd_command(0x3A); // Interface Pixel Format
-        self.lcd_data(0x55);    // 16-bit color format
+        self.lcd_data(0x55); // 16-bit color format
 
         self.lcd_command(0xB2); // Porch Setting
-        self.lcd_data(0x0C); 
-        self.lcd_data(0x0C); 
-        self.lcd_data(0x00); 
-        self.lcd_data(0x33); 
+        self.lcd_data(0x0C);
+        self.lcd_data(0x0C);
+        self.lcd_data(0x00);
+        self.lcd_data(0x33);
         self.lcd_data(0x33);
 
         self.lcd_command(0xB7); // Gate Control
-        self.lcd_data(0x35);    // Default value
+        self.lcd_data(0x35); // Default value
 
         self.lcd_command(0xBB); // VCOM Setting
-        self.lcd_data(0x19);    // Default value
+        self.lcd_data(0x19); // Default value
 
         self.lcd_command(0xC0); // LCM Control
-        self.lcd_data(0x2C);    // Default value
+        self.lcd_data(0x2C); // Default value
 
         self.lcd_command(0xC2); // VDV and VRH Command Enable
-        self.lcd_data(0x01);    // Default value
+        self.lcd_data(0x01); // Default value
 
         self.lcd_command(0xC3); // VRH Set
-        self.lcd_data(0x12);    // Default value
+        self.lcd_data(0x12); // Default value
 
         self.lcd_command(0xC4); // VDV Set
-        self.lcd_data(0x20);    // Default value
+        self.lcd_data(0x20); // Default value
 
         self.lcd_command(0xC6); // Frame Rate Control in Normal Mode
-        self.lcd_data(0x0F);    // Default value
+        self.lcd_data(0x0F); // Default value
 
         self.lcd_command(0xD0); // Power Control
-        self.lcd_data(0xA4);    // Default value
-        self.lcd_data(0xA1);    // Default value
+        self.lcd_data(0xA4); // Default value
+        self.lcd_data(0xA1); // Default value
 
         self.lcd_command(0xE0); // Positive Voltage Gamma Control
-        self.lcd_data(0xD0); 
-        self.lcd_data(0x04); 
-        self.lcd_data(0x0D); 
-        self.lcd_data(0x11); 
-        self.lcd_data(0x13); 
-        self.lcd_data(0x2B); 
-        self.lcd_data(0x3F); 
-        self.lcd_data(0x54); 
-        self.lcd_data(0x4C); 
-        self.lcd_data(0x18); 
-        self.lcd_data(0x0D); 
-        self.lcd_data(0x0B); 
-        self.lcd_data(0x1F); 
+        self.lcd_data(0xD0);
+        self.lcd_data(0x04);
+        self.lcd_data(0x0D);
+        self.lcd_data(0x11);
+        self.lcd_data(0x13);
+        self.lcd_data(0x2B);
+        self.lcd_data(0x3F);
+        self.lcd_data(0x54);
+        self.lcd_data(0x4C);
+        self.lcd_data(0x18);
+        self.lcd_data(0x0D);
+        self.lcd_data(0x0B);
+        self.lcd_data(0x1F);
         self.lcd_data(0x23);
 
         self.lcd_command(0xE1); // Negative Voltage Gamma Control
-        self.lcd_data(0xD0); 
-        self.lcd_data(0x04); 
-        self.lcd_data(0x0C); 
-        self.lcd_data(0x11); 
-        self.lcd_data(0x13); 
-        self.lcd_data(0x2C); 
-        self.lcd_data(0x3F); 
-        self.lcd_data(0x44); 
-        self.lcd_data(0x51); 
-        self.lcd_data(0x2F); 
-        self.lcd_data(0x1F); 
-        self.lcd_data(0x1F); 
-        self.lcd_data(0x20); 
+        self.lcd_data(0xD0);
+        self.lcd_data(0x04);
+        self.lcd_data(0x0C);
+        self.lcd_data(0x11);
+        self.lcd_data(0x13);
+        self.lcd_data(0x2C);
+        self.lcd_data(0x3F);
+        self.lcd_data(0x44);
+        self.lcd_data(0x51);
+        self.lcd_data(0x2F);
+        self.lcd_data(0x1F);
+        self.lcd_data(0x1F);
+        self.lcd_data(0x20);
         self.lcd_data(0x23);
 
         self.lcd_command(0x21); // Inversion On
